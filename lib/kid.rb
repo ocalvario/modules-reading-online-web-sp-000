605de50d21d7b9ec2a21fafa4f3c46d4cfecbb1d
Oswald Calvario <oswald.calvario@gmail.com>
2020-05-10 03:41:12 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,11 +1,15 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './class_methods_module.rb'
 
 class Kid
  
  include Dance
  
  extend MetaDancing
+ 
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
  
   attr_accessor :name
  
